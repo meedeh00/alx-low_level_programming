ef517223969253f37cfea2a073fed5536a342bdd
meedeh00 <meedehtayo69@gmail.com>
2023-07-17 08:13:12 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,7 +18,7 @@ int main(void)
 			{
 				putchar(num_1);
 				putchar(num_2);
-				if (num_1 != '8' || (num_1 == '8' && num_2 != '9'))
+				if (num_1 != '8' || (num_1 == '8' && num_2 != '0'))
 				{
 					putchar(',');
 					putchar(' ');
@@ -27,6 +27,7 @@ int main(void)
 			num_2++;
 		}
 		num_1++;
+		NUM_2 = '0';
 	}
 	putchar('\n');
 	return (0);
