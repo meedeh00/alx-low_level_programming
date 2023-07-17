c334e1da21015937487eb287d2732d1681af5dce
meedeh00 <meedehtayo69@gmail.com>
2023-07-17 04:50:33 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print numbers in base ten
+ * Return: always (0)
+ */
+int main(void)
+{
+	int base_ten = 0;
+
+	while (base_ten <= 10)
+	{
+		putchar(base_ten);
+		base_ten++;
+	}
+	putchar('\n');
+	return (0);
+}
