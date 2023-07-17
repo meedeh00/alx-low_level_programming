fc1bcb72a0fc6057943ba80dd26d502d94e2a5ac
meedeh00 <meedehtayo69@gmail.com>
2023-07-17 03:35:13 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print in caps and lower case
+ * Return: 0 (working)
+ */
+int main(void)
+{
+	char Lower = 'a';
+	char Upper = 'A';
+
+	while (lower < = 'z')
+	{
+		putchar(lower);
+		Lower++;
+	}
+	while (upper < = 'z')
+	{
+		putchar(upper);
+		Upper++;
+	}
+	putchar('\n');
+	return (0);
+}
