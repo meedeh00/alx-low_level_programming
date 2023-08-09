857d8536284ed20e983742d6b44dd4fff9f27726
meedeh00 <meedehtayo69@gmail.com>
2023-08-09 14:33:34 -0700

    ff

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+_puts("At the end of the day, my goal is to be the best hacker - Kevin Mit");
+return (0);
 }
