ed60430cfe4a2f6d44b1f2347b11359bd957a8b9
meedeh00 <meedehtayo69@gmail.com>
2023-08-09 14:20:21 -0700

    real

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -5,13 +5,12 @@
  *@src: memory where is copied
  *@n: number of bytes
  *
- *Return: copied memory with n byted changed
+ *Return: copied memory with n bytes changed
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int r = 0;
 	int i = n;
-
 
 	for (; r < i; r++)
 	{
